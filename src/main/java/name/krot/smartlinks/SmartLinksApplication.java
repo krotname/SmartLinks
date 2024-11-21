5919c1a0360c56f11571a80dd13248c1c25a62d2
Ovcharenko.AA <Ovcharenko.AA@esphere.ru>
2024-11-21 23:49:08 +0300

    init

---
+++ src/main/java/name/krot/smartlinks/SmartLinksApplication.java
@@ -0,0 +1,13 @@
+package name.krot.smartlinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLinksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLinksApplication.class, args);
+    }
+
+}
