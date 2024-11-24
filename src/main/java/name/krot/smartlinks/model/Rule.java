b961e4cd9a36f2c394472bf8490858ea0107777d
Ovcharenko.AA <Ovcharenko.AA@esphere.ru>
2024-11-25 00:54:45 +0300

    Rule validate add

--- src/main/java/name/krot/smartlinks/model/Rule.java
+++ src/main/java/name/krot/smartlinks/model/Rule.java
@@ -1,5 +1,9 @@
 package name.krot.smartlinks.model;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.io.Serial;
@@ -11,8 +15,12 @@ import java.util.Map;
 public class Rule implements Serializable {
     @Serial
     private static final long serialVersionUID = -4862926644813433701L;
-
+    @NotEmpty(message = "Predicates list cannot be empty")
     private List<String> predicates;
+    @NotNull(message = "Args cannot be null")
     private Map<String, Object> args;
+    @NotNull(message = "Redirect URL cannot be null")
+    @Size(max = 2048, message = "Redirect URL cannot exceed 2048 characters")
+    @Pattern(regexp = "^(http|https)://.*$", message = "Redirect URL must be a valid URL")
     private String redirectTo;
 }
